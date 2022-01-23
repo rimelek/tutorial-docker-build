b264e9b53f2466cc6c2fd0e845e4c83e7d639ea4
Takács Ákos <rimelek@rimelek.hu>
2022-01-23 18:28:31 +0100

    add dockerdb-reader wrapper script
    it copies the original database files,
    so it can be read without stopping docker
    I couldn't find better solution

---
+++ scripts/docker-db-reader.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+source "$script_dir/env.sh"
+[[ -f "$script_dir/env.custom.sh" ]] && source "$script_dir/env.custom.sh"
+
+dir_target_base="$script_dir/../var/dockerdb-tmp"
+
+file_src=$1
+file_src_abs_path="$(cd "$(dirname "$file_src")" && pwd)/$(basename "$file_src")"
+file_target_abs_path="${dir_target_base}${file_src_abs_path}"
+
+file_target_abs_dir="$(dirname "$file_target_abs_path")"
+
+mkdir -p "$file_target_abs_dir"
+
+sudo cp "$file_src_abs_path" "$file_target_abs_path"
+
+"$script_dir/../var/bin/dockerdb-reader" "$file_target_abs_path"
