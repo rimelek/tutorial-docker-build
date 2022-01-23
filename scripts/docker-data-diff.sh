253b4cb2d9567c6175148ee0775170c96148557b
Takács Ákos <rimelek@rimelek.hu>
2022-01-23 21:15:09 +0100

    add diff helper scripts

---
+++ scripts/docker-data-diff.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail 
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+source "$script_dir/env.sh"
+[[ -f "$script_dir/env.custom.sh" ]] && source "$script_dir/env.custom.sh"
+
+dir_current="$PROJECT_DOCKER_DATA_DIR"
+dir_archived_base="$PROJECT_DOCKER_DATA_DIR_ARCHIVED_BASE"
+
+version_1="$1"
+version_2=""
+
+if (( $# > 1 )); then
+  version_2="$2"
+else
+  version_2="$version_1"
+  version_1="0"
+fi
+
+dir_1="$dir_archived_base.$version_1"
+if [[ "$version_1" == "0" ]]; then
+  dir_1="$dir_current"
+fi
+
+dir_2="$dir_archived_base.$version_2"
+
+function db_files_rel() {
+  echo "volumes/metadata.db"
+  echo "network/files/local-kv.db"
+  echo "buildkit/snapshots.db"
+  echo "buildkit/containerdmeta.db"
+  echo "buildkit/cache.db"
+  echo "buildkit/metadata_v2.db"
+}
+
+function db_files_pattern() {
+  db_files_rel \
+    | sed "s#^#$dir_1#" \
+    | sed 's#$#|#' \
+    | sed 's/\./\\./g' \
+    | sed 's/|/\\|/g' \
+    | tr -d $'\n'
+}
+
+sudo diff --no-dereference -rq "$dir_1" "$dir_2" 2>/dev/stdout \
+  | grep -E -v 'block special file|character special file' \
+  | grep -v "$(db_files_pattern "$dir_1")" \
+  || true
+
+
+# diff the actual content of the databases as json, not the binaries
+db_files_rel | xargs --replace='{}' -- "$script_dir/docker-db-diff.sh" -q "$dir_1/{}" "$dir_2/{}"  
+
---
+++ scripts/docker-db-diff.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# https://stackoverflow.com/a/31933234/2584843
+
+set -eu -o pipefail
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+
+quiet=0
+
+while getopts ':q' opt; do
+  case "$opt" in
+    q) quiet=1 ;; # don't show the actual diff, only the filenames 
+    *)
+      >&2 echo "Invalid flag: -$opt"
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+file_a="$1"
+file_b="$2"
+
+if [[ "$quiet" == "1" ]]; then
+  diff -q \
+    <("$script_dir/docker-db-reader.sh" "$file_a") \
+    <("$script_dir/docker-db-reader.sh" "$file_b") \
+    1>/dev/null \
+    || echo "Files $file_a and $file_b differ"
+else
+  diff \
+    <("$script_dir/docker-db-reader.sh" "$file_a") \
+    <("$script_dir/docker-db-reader.sh" "$file_b")
+fi
+
+
+# alternative solution
+# jq \
+#   --argjson a "$(cat a.json)" \
+#   --argjson b "$(cat c.json)" \
+#   -n \
+#   '($a | (.. | arrays) |= sort) as $a | ($b | (.. | arrays) |= sort) as $b | $a == $b'
+
