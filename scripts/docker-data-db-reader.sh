d04e36bc4bbfcd808d62f6adeb83aeb71954a10e
Takács Ákos <rimelek@rimelek.hu>
2022-01-26 22:17:09 +0100

    add docker-data-db-reader.sh

---
+++ scripts/docker-data-db-reader.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+source "$script_dir/env.sh"
+[[ -f "$script_dir/env.custom.sh" ]] && source "$script_dir/env.custom.sh"
+
+"$script_dir/docker-db-reader.sh" "$PROJECT_DOCKER_DATA_DIR/$1"
