7612b5758674efb662849a0416d32947b317cc68
Takács Ákos <rimelek@rimelek.hu>
2022-01-29 19:57:50 +0100

    use docker-data-cat.sh in docker-data-repositories.sh

--- scripts/docker-data-repositories.sh
+++ scripts/docker-data-repositories.sh
@@ -3,7 +3,5 @@
 set -eu -o pipefail
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
-source "$script_dir/env.sh"
-[[ -f "$script_dir/env.custom.sh" ]] && source "$script_dir/env.custom.sh"
 
-sudo cat "$PROJECT_DOCKER_DATA_DIR/image/overlay2/repositories.json" | jq .
+"$script_dir/docker-data-cat.sh" "image/overlay2/repositories.json" | jq .
