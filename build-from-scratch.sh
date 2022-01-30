d6538f874b6597a18c8fd465362eb2a6a15cf31a
Takács Ákos <rimelek@rimelek.hu>
2022-01-30 01:51:31 +0100

    fix Metadata.LastTagTime

--- build-from-scratch.sh
+++ build-from-scratch.sh
@@ -6,22 +6,22 @@ set -eu -o pipefail
 
 docker_root_dir=/var/lib/docker
 storage_driver=overlay2
-meta_path_dst_dir="$docker_root_dir/image/$storage_driver/imagedb/content/sha256"
-last_updated_dst_dir="$docker_root_dir/image/$storage_driver/imagedb/metadata/sha256/"
+imagedb_content_dst_dir="$docker_root_dir/image/$storage_driver/imagedb/content/sha256"
+imagedb_metadata_dst_dir="$docker_root_dir/image/$storage_driver/imagedb/metadata/sha256"
 repositories_path="$docker_root_dir/image/$storage_driver/repositories.json"
 
 # Image Layer settings
-
 meta_path_src="meta.json"
 meta_json_compact="$(jq -c . "$meta_path_src")"
 image_id="$(echo "$meta_json_compact" | sha256sum | cut -d " " -f 1)"
+last_updated_dst_dir="$imagedb_metadata_dst_dir/$image_id"
 
 # Image layer creation
-echo "$meta_json_compact" > "$meta_path_dst_dir/$image_id"
+echo "$meta_json_compact" > "$imagedb_content_dst_dir/$image_id"
+mkdir -p "$last_updated_dst_dir"
 date +%Y-%m-%dT%H:%M:%S.%N%:z | tr -d '\n' > "$last_updated_dst_dir/lastUpdated"
 
 # Add a tag to the layer
-
 repository="localhost/buildtest"
 tag="v7"
 
