f1c802b9eec4e33e464d065d8bb0c587b0d12570
Takács Ákos <rimelek@rimelek.hu>
2022-02-01 20:25:26 +0100

    add status notes to the data destroy script

--- scripts/docker-data-destroy.sh
+++ scripts/docker-data-destroy.sh
@@ -11,9 +11,11 @@ echo -n "Are you sure you want to destroy \"$dir\"? [y/N] "
 read confirm
 
 if [[ "$confirm" == "y" ]]; then
-  echo "Destroying \"$dir\" ..."
+  echo "Stopping Docker daemon: sudo systemctl stop docker.service docker.socket"
   sudo systemctl stop docker.service docker.socket
+  echo "Destroying \"$dir\" ..."
   sudo rm -rf "$dir"
+  echo "Starting Docker daemon"
   sudo systemctl start docker.service docker.socket
   echo "Docker data destroyed"
 else
