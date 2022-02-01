ba60de5e835338b7c91ad962e2f08d59975092ea
Takács Ákos <rimelek@rimelek.hu>
2022-02-01 18:32:10 +0100

    fix project cleaner's shebang line and delete custom config

--- scripts/project-clean.sh
+++ scripts/project-clean.sh
@@ -1,5 +1,6 @@
-#!/usr/bn/env
+#!/usr/bin/env bash
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 
 find "$script_dir/../var/bin/" -type f -not -name .gitkeep -exec unlink {} \;
+unlink "$script_dir/../env.custom.sh"
